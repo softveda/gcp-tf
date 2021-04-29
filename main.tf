eb59f167abe22d5d1f900e5b21dc6291e8a7d10d
Pratik Khasnabis <pratik.khasnabis@outlook.com>
2021-04-29 22:46:32 +1000

    Terraform project to provision in Google Cloud

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.66.0"
+    }
+  }
+}
+
+provider "google" {
+  project     = "pratik-sandbox"
+  region      = "us-central1"
+  zone        = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
