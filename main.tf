85a1edabdd7b1270c80c64cf95d9afb36eb81408
Pratik Khasnabis <pratik.khasnabis@outlook.com>
2021-04-30 11:13:11 +1000

    Adding Terraform action to provision in GCP

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
 terraform {
+  backend "remote" {
+    organization = "pratik-hc"
+
+    workspaces {
+      name = "github-gcp-tf"
+    }
+  }
   required_providers {
     google = {
       source  = "hashicorp/google"
@@ -6,11 +13,10 @@ terraform {
     }
   }
 }
-
 provider "google" {
-  project     = "pratik-sandbox"
-  region      = "us-central1"
-  zone        = "us-central1-c"
+  project = "pratik-sandbox"
+  region  = "us-central1"
+  zone    = "us-central1-c"
 }
 
 resource "google_compute_network" "vpc_network" {
