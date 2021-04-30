3743934b15dd1cfa519d42df2484f2411b7f222d
Pratik Khasnabis <pratik.khasnabis@outlook.com>
2021-04-30 16:11:48 +1000

    Added vm resource and running on custom agent

--- main.tf
+++ main.tf
@@ -20,5 +20,38 @@ provider "google" {
 }
 
 resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
+  name = "vault-network"
+}
+
+variable "ssh_pub_key" {
+  type = string
+}
+
+variable "service_account_email" {
+  type = string
+}
+resource "google_compute_instance" "vm_instance" {
+  name         = "vault-01"
+  machine_type = "n1-standard-1"
+  boot_disk {
+    initialize_params {
+      image = "ubuntu/ubuntu-20.04"
+    }
+  }
+  metadata = {
+    ssh-keys = var.ssh_pub_key
+  }
+  network_interface {
+    access_config {
+    }
+    network = google_compute_network.vpc_network.self_link
+  }
+
+  allow_stopping_for_update = true
+
+  /* service_account {
+    email  = var.service_account_email
+    scopes = ["cloud-platform"]
+  } */
+
 }
